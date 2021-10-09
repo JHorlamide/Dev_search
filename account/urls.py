05bdd6ea11c189f051aa4267e655775ab3080b32
Horlamide <olamide.jubril@taxtech.com.ng>
2021-10-09 11:56:44 +0100

    User account edit

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.profiles, name='profiles'),
     path('profile/<uuid:pk>', views.userProfile, name='user_profile'),
